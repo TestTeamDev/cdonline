6c7e75684eb40865f0e7fc4a4e8e71c21e2a77bb
matsukazu0918 <kazu.baske74@gmail.com>
2019-11-13 10:13:44 +0000

    matsu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'cds/index'
+  get 'cds/show'
+  get 'cds/edit'
+  get 'cds/new'
   devise_for :admins
   devise_for :endusers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
