5104a44d7995630063b2b6f7f6c5bec8f2d6dc39
matsukazu0918 <kazu.baske74@gmail.com>
2019-11-13 12:02:29 +0000

    cntroller

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,23 +1,23 @@
 class ArtistsController < ApplicationController
 
- 　def index
-   end
-　 def create
-   	artist = Artist.new(artist_params)
-   	artist.save
-   	#redirect_to '/top'
-   end
-   def destroy
-   end
 
+	def index
+	end
+	def create
+		artist = Artist.new(artist_params)
+   	    artist.save
+   	    #redirect_to '/top'
+   	end
+   	def destroy
+   	end
+
+
+
+
+	private
+
+	 def artist_params
+	 	params.require(:artist).permit(:name)
+	 end
 
 end
-
-
-
-
-private
-
- def artist_params
- 	params.require(:artist).permit(:name)
- end
