400ec2e92947003401b55296a07242c0341ab871
yuzu753 <nakago0513@gmail.com>
2019-11-13 06:55:23 +0000

    add model  cart_item/inquire

---
+++ db/migrate/20191113054003_create_cart_items.rb
@@ -0,0 +1,11 @@
+class CreateCartItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_items do |t|
+      t.integer :enduser_id
+      t.integer :cd_id
+      t.integer :item_count
+      t.datetime :created_at
+      t.datetime :created_at
+    end
+  end
+end
