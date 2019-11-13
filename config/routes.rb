d83b29861732a1123f047e8a6898a84451cf9e59
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2019-11-13 10:16:27 +0000

    adminルーティング途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+namespace :admins do
   devise_for :admins
+end
   devise_for :endusers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
