04c02786d2e2e3c610951792da2d4744998b7b01
yuzu753 <nakago0513@gmail.com>
2019-11-13 10:11:02 +0000

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'endusers/index'
+  get 'endusers/show'
+  get 'endusers/edit'
+  get 'endusers/leave'
   devise_for :admins
   devise_for :endusers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
