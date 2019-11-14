a8638177ba2f1ebf083c2b81c72b4f564911336f
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2019-11-14 07:29:32 +0000

    admin-login-fin-zaki

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+      case resource
+	      when Enduser
+	        root_path
+	      when Admin
+	        admin_cds_path
+	  end
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    case resource
+	      when Enduser
+	        root_path
+	      when Admin
+	        new_admins_admin_session_path
+	end
+  end
 end
