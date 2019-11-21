23fce651ef6e85664a3a562ef389f2ff34868e54
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2019-11-21 05:40:49 +0000

    order-newdel-saveしたい

--- app/controllers/enduser/orders_controller.rb
+++ app/controllers/enduser/orders_controller.rb
@@ -1,6 +1,6 @@
 class Enduser::OrdersController < ApplicationController
 	def index
-		@orders = current_endusers_enduser.orders.all
+		@orders = current_endusers_enduser.orders.all.page(params[:page]).per(4)
 		@enduser = Enduser.find(current_endusers_enduser.id)
 	end
 
@@ -16,18 +16,27 @@ class Enduser::OrdersController < ApplicationController
 
         #エンドユーザー外部キー情報の受け渡し
         order.enduser_id = current_endusers_enduser.id
+        if params[:address_button].to_i == 1
 
-        #order_confomationで選んだdelivey_addressの情報を受け取る
+            #order_confomationで選んだdelivey_addressの情報を受け取る
+        	delivery_address = DeliveryAddress.find_by(id: params[:address_id])  #find(params[:order][:address)でも可
 
-        delivery_address = DeliveryAddress.find_by(address: params[:order][:address])
-        if DeliveryAddress.find_by(address: params[:order][:address]).present?
-			order.first_name = delivery_address.first_name
-			order.last_name = delivery_address.last_name
-			order.postcode = delivery_address.postcode
-			order.address = delivery_address.address
-			order.postage = @postage.postage
-	    end
-
+        	if DeliveryAddress.find_by(id: params[:address_id]).present?
+				order.first_name = delivery_address.first_name
+				order.last_name = delivery_address.last_name
+				order.postcode = delivery_address.postcode
+				order.address = delivery_address.address
+				order.postage = @postage.postage
+	    	end
+		elsif  params[:address_button].to_i == 2
+           order.postage = @postage.postage
+     #       new_address = DeliveryAdress.new
+		   # new_address.enduser_id = current_endusers_enduser.id
+		   # new_address.postcode = order.postcode
+		   # new_address.address = order.address
+		   # new_address.save
+        else
+		end
 
 		#購入商品情報と購入数、小計を保存
 		cart_items.each do |c|
@@ -52,7 +61,8 @@ class Enduser::OrdersController < ApplicationController
             end
 		  redirect_to enduser_order_confirmations_thanks_path
 		else
-			render 'enduser/cart_items/index'
+		  flash[:create_false]  = "配送先を指定してください"
+		  redirect_to enduser_order_confirmations_path
 		end
 
 	end
