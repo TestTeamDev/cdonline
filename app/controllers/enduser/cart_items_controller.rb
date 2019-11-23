9afb9f080fa6235dc202b47beeac3a8c04bfbbc9
yuzu753 <nakago0513@gmail.com>
2019-11-23 07:30:19 +0000

    admin view

--- app/controllers/enduser/cart_items_controller.rb
+++ app/controllers/enduser/cart_items_controller.rb
@@ -19,15 +19,15 @@ class Enduser::CartItemsController < ApplicationController
     redirect_to enduser_cart_items_path
   end
 
-  def destroy
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
-    redirect_to enduser_cart_items_path
-　end
-
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
+    redirect_to enduser_cart_items_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
     redirect_to enduser_cart_items_path
   end
 
